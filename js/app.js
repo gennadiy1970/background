84690c5a4cb7291ab16b9d57eebebab62c947184
gennadiy1970 <ggm_kharkiv@ukr.net>
2018-11-07 13:08:05 +0200

    fix origin

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+const res = document.querySelector('.res');
+const range = document.querySelector('#range');
+const resY = document.querySelector('.resY');
+const rangeY = document.querySelector('#rangeY');
 const elemBG = document.querySelector('.bg');
 const elemSize = document.querySelector('.bg-size');
 const elemRepeat = document.querySelector('.bg-repeat');
@@ -9,6 +13,21 @@ const liPosition = document.querySelectorAll('.bg-position .prop');
 elemSize.addEventListener('click', changeSize, false);
 elemRepeat.addEventListener('click', changeRepeat, false);
 elemPosition.addEventListener('click', changePosition, false);
+range.addEventListener('change', changeRange, false);
+rangeY.addEventListener('change', changeRangeY, false);
+
+function changeRange (e) {
+    e.preventDefault();
+    resY.textContent = ''
+    res.textContent = range.value.length < 2 ? '0' +  range.value : range.value;
+    elemBG.style.backgroundSize = range.value + '%' + ' ' + 'auto';
+}
+function changeRangeY (e) {
+    e.preventDefault();
+    res.textContent = '';
+    resY.textContent = rangeY.value.length < 2 ? '0' +  rangeY.value : rangeY.value;
+    elemBG.style.backgroundSize = 'auto' + ' ' + rangeY.value + '%';
+}
 
 function setActive(elem, list) {
     Array.from(list, e => e.classList.remove('active'));
